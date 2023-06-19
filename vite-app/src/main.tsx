da3b81a1dc4e0296c4130c3744a18bcb0a56568e
Hristo Kolev <hristo.a.kolev@gmail.com>
2023-06-19 17:41:38 +0300

    rename index.css to main.css

--- vite-app/src/main.tsx
+++ vite-app/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
-import './index.css';
+import './main.css';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
