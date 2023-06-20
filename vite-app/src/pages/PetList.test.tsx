1f722f8c13149a8f7ddce385ae1f882f421d80d0
Hristo Kolev <hristo.a.kolev@gmail.com>
2023-06-20 19:54:38 +0300

    add more PetList tests

--- vite-app/src/pages/PetList.test.tsx
+++ vite-app/src/pages/PetList.test.tsx
@@ -1,5 +1,6 @@
 import { afterEach, test, vi } from 'vitest';
 import { cleanup, render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { mockPetKindsByValue, mockPetList } from '~testing/mock-data.ts';
 
@@ -48,4 +49,52 @@ test('row shows pet list item data', async ({ expect }) => {
     '31 Oct 2022'
   );
   expect(within(row).getByTestId('col_petKind')).toHaveTextContent('Cat');
+});
+
+test('calls onDelete when the user clicks on the delete button', async ({
+  expect,
+}) => {
+  const user = userEvent.setup();
+
+  const handleOnDelete = vi.fn();
+
+  render(
+    <PetList
+      petList={mockPetList}
+      petKindsByValue={mockPetKindsByValue}
+      onEdit={vi.fn}
+      onDelete={handleOnDelete}
+    />
+  );
+
+  const table = await screen.findByRole('table');
+  const row = within(table).getAllByRole('row', { name: 'Pet' })[0];
+
+  await user.click(within(row).getByRole('button', { name: 'Delete' }));
+
+  expect(handleOnDelete).toBeCalledWith(42);
+});
+
+test('calls onEdit when the user clicks on the edit button', async ({
+  expect,
+}) => {
+  const user = userEvent.setup();
+
+  const handleOnEdit = vi.fn();
+
+  render(
+    <PetList
+      petList={mockPetList}
+      petKindsByValue={mockPetKindsByValue}
+      onEdit={handleOnEdit}
+      onDelete={vi.fn}
+    />
+  );
+
+  const table = await screen.findByRole('table');
+  const row = within(table).getAllByRole('row', { name: 'Pet' })[0];
+
+  await user.click(within(row).getByRole('button', { name: 'View / Edit' }));
+
+  expect(handleOnEdit).toBeCalledWith(42);
 });
