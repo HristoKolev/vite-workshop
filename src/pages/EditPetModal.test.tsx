ddaff407b209cc7af7b0f39c722820740a449492
Hristo Kolev <hristo.a.kolev@gmail.com>
2023-12-29 19:34:29 +0200

    add missing test

--- src/pages/EditPetModal.test.tsx
+++ src/pages/EditPetModal.test.tsx
@@ -561,4 +561,25 @@ describe('add mode', () => {
 
     expect(handleOnClose).toBeCalled();
   });
+
+  test('once pet kind is selected the empty option is removed from the list', async ({
+    expect,
+  }) => {
+    const user = userEvent.setup();
+
+    await renderEditModal({ addMode: true });
+
+    const kindField = screen.getByLabelText('Kind:');
+
+    const getOptionValues = () =>
+      within(kindField)
+        .getAllByRole<HTMLOptionElement>('option')
+        .map((x) => x.value);
+
+    expect(getOptionValues()).toStrictEqual(['', '1', '2', '3']);
+
+    await user.selectOptions(kindField, ['Cat']);
+
+    expect(getOptionValues()).toStrictEqual(['1', '2', '3']);
+  });
 });
